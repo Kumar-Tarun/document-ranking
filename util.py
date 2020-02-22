e4f8b810bca265a54195a5858f62208bb32e33b8
Tarun Kumar <kumar1998.tarun@gmail.com>
2020-02-22 21:54:04 +0530

    Add function for computing doc lengths.

--- util.py
+++ util.py
@@ -15,9 +15,13 @@ def construct_inv_index(path):
 	soup = BeautifulSoup(text, 'html.parser')
 	docs = soup.find_all('doc')
 	inverted_index = {}
+	doc_id_2_title = {}
 	for match in docs:
 		doc_text = match.get_text().strip().lower()
 		doc_id = int(match.get('id'))
+		doc_title = match.get('title')
+		doc_id_2_title[doc_id] = doc_title
+
 		for tok in nlp(doc_text, disable=['tagger', 'parser', 'ner']):
 			if tok.text in set(string.punctuation):
 				continue
@@ -33,6 +37,9 @@ def construct_inv_index(path):
 
 	with open('inv_index.pkl', 'wb+') as f:
 		pickle.dump(inv_index, f)
+
+	with open('doc_id_2_title.pkl', 'wb+') as f:
+		pickle.dump(doc_id_2_title, f)
 
 	print(len(inv_index.keys()))
 
@@ -53,12 +60,27 @@ def compute_doc_vectors(index):
 
 	print(len(doc_vectors.keys()))
 
-	# print(doc_vectors[doc_vectors.keys()[:5]])
+def compute_doc_lengths(index):
+	doc_lengths = {}
+	vocab_size = len(index.keys())
+	for term, dic in index.items():
+		for doc, tf in dic.items():
+			wtd = 1 + np.log10(tf)
+			if doc not in doc_lengths:
+				doc_lengths[doc] = []
+				doc_lengths[doc].append(wtd)
+			else:
+				doc_lengths[doc].append(wtd)
+
+	for doc_id, lis in doc_lengths.items():
+		doc_lengths[doc_id] = (np.array(lis)**2).sum()
+
+	with open('doc_lengths.pkl', 'wb+') as f:
+		pickle.dump(doc_lengths, f)
+
+	print(len(doc_lengths.keys()))
 
 if __name__ == "__main__":
 	index = construct_inv_index('./wiki_10')
-	compute_doc_vectors(index)
-	# with open('doc_vectors.pkl', 'rb') as f:
-	# 	dic = pickle.load(f)
-
-	# print(len(dic.keys()))
+	# compute_doc_vectors(index)
+	compute_doc_lengths(index)
