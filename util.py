50531f52add52736a306f57d09320e52f8936cc9
Tarun Kumar <kumar1998.tarun@gmail.com>
2020-05-16 00:53:40 +0530

    Add functions for spell correction and bigram index.

--- util.py
+++ util.py
@@ -3,11 +3,21 @@ import spacy
 import pickle
 import string
 import numpy as np
+from tqdm import tqdm
 from bs4 import BeautifulSoup
 
 nlp = spacy.load('en_core_web_sm')
 
-def construct_inv_index(path):
+def construct_inv_index(path, bi=False):
+	'''
+	:param path: path to corpus file
+	:param bi: add bigrams to inverted index or not
+
+	:returns:
+	inverted-index
+
+	saves inverted-index and doc-2-title mapping onto disk
+	'''
 
 	file = open(path, 'r')
 	# doc_start_regex = re.compile(r'<doc id=\"(?P<doc_id>.+?)\".+?title=\"(?P<doc_title>.+?)\">'))
@@ -16,54 +26,63 @@ def construct_inv_index(path):
 	docs = soup.find_all('doc')
 	inverted_index = {}
 	doc_id_2_title = {}
-	for match in docs:
+	for match in tqdm(docs):
 		doc_text = match.get_text().strip().lower()
+
+		# remove punctuations
+		doc_text = doc_text.translate(str.maketrans('', '', string.punctuation))
+
 		doc_id = int(match.get('id'))
 		doc_title = match.get('title')
+
+		# map doc-id to title 
 		doc_id_2_title[doc_id] = doc_title
 
-		for tok in nlp(doc_text, disable=['tagger', 'parser', 'ner']):
-			if tok.text in set(string.punctuation):
-				continue
-			if tok.text in inverted_index:
-				if doc_id in inverted_index[tok.text]:
-					inverted_index[tok.text][doc_id] += 1
-				else:
-					inverted_index[tok.text][doc_id] = 1
-			else:
-				inverted_index[tok.text] = {doc_id: 1}
+		doc_text = nlp(doc_text, disable=['tagger', 'parser', 'ner'])
+		stop = len(doc_text)
+		for i in range(stop):
+			term = doc_text[i].text.strip()
 
-	inv_index = {k:v for k, v in sorted(inverted_index.items(), key=lambda x: x[0])}
+			if term in inverted_index:
+				if doc_id in inverted_index[term]:
+					inverted_index[term][doc_id] += 1
+				else:
+					inverted_index[term][doc_id] = 1
+			else:
+				inverted_index[term] = {doc_id: 1}
+
+			# add bigrams to inverted-index if bi is true
+			if bi and i != stop-1:
+				term1 = doc_text[i+1].text.strip()
+				term1 = (term, term1)
+				if term1 in inverted_index:
+					if doc_id in inverted_index[term1]:
+						inverted_index[term1][doc_id] += 1
+					else:
+						inverted_index[term1][doc_id] = 1
+				else:
+					inverted_index[term1] = {doc_id: 1}
 
 	with open('inv_index.pkl', 'wb+') as f:
-		pickle.dump(inv_index, f)
+		pickle.dump(inverted_index, f)
 
 	with open('doc_id_2_title.pkl', 'wb+') as f:
 		pickle.dump(doc_id_2_title, f)
 
-	print(len(inv_index.keys()))
-
-	return inv_index
-
-def compute_doc_vectors(index):
-	doc_vectors = {}
-	vocab_size = len(index.keys())
-	for i, (term, dic) in enumerate(index.items()):
-		for doc, tf in dic.items():
-			if doc not in doc_vectors:
-				doc_vectors[doc] = np.zeros((vocab_size, 1), dtype=np.float16)
-
-			doc_vectors[doc][i] = 1 + np.log10(tf, dtype=np.float16)
-
-	with open('doc_vectors.pkl', 'wb+') as f:
-		pickle.dump(doc_vectors, f)
-
-	print(len(doc_vectors.keys()))
+	return inverted_index
 
 def compute_doc_lengths(index):
+	'''
+	:param index: inverted-index
+
+	saves unigram doc lengths onto disk
+	'''
 	doc_lengths = {}
+	i = 0
 	vocab_size = len(index.keys())
 	for term, dic in index.items():
+		if isinstance(term, tuple):
+			continue
 		for doc, tf in dic.items():
 			wtd = 1 + np.log10(tf)
 			if doc not in doc_lengths:
@@ -77,9 +96,43 @@ def compute_doc_lengths(index):
 	with open('doc_lengths.pkl', 'wb+') as f:
 		pickle.dump(doc_lengths, f)
 
-	print(len(doc_lengths.keys()))
+def compute_bi_doc_lengths(index):
+	'''
+	:param index: inverted-index
+
+	saves bigram doc lengths onto disk
+	'''
+	doc_lengths = {}
+	vocab_size = len(index.keys())
+	for term, dic in index.items():
+		if not isinstance(term, tuple):
+			continue
+		for doc, tf in dic.items():
+			wtd = 1 + np.log10(tf)
+			if doc not in doc_lengths:
+				doc_lengths[doc] = []
+
+			doc_lengths[doc].append(wtd)
+
+	for doc_id, lis in doc_lengths.items():
+		doc_lengths[doc_id] = (np.array(lis)**2).sum()
+
+	with open('doc_bi_lengths.pkl', 'wb+') as f:
+		pickle.dump(doc_lengths, f)
+
+
+def main():
+	path = input('Enter path to corpus file: ')
+	inp = input('Enter 1 for Part1 and Part2, Improvement1\nEnter 2 for Part2, Improvement2\nResponse: ')
+	if(inp == '1'):
+		index = construct_inv_index(path)
+		compute_doc_lengths(index)
+	elif(inp == '2'):
+		index = construct_inv_index(path, bi=True)
+		compute_doc_lengths(index)
+		compute_bi_doc_lengths(index)
+	else:
+		print('This option is not supported.')
 
 if __name__ == "__main__":
-	index = construct_inv_index('./wiki_10')
-	# compute_doc_vectors(index)
-	compute_doc_lengths(index)
+	main()
